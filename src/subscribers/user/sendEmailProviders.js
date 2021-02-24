b145f3b4e3689b0bcfd107120fd2449bf091808b
jcorvera <javier.corvra@gmail.com>
2021-02-24 17:28:43 -0600

    update users profiles

--- src/subscribers/user/sendEmailProviders.js
+++ src/subscribers/user/sendEmailProviders.js
@@ -1,10 +1,10 @@
 const {transport} = require('../../config/email/index');
 const {sendCodeForVerifyEmail, sendLinkForResetPassword} = require('../../services/notifications/emailNotification');
 
-function mailSubscriber(myEmitter) {
+function sendEmailProviders(myEmitter) {
     myEmitter.once('signup-providers',  function (data) {  // event listener
         sendCodeForVerifyEmail(transport, data).catch(console.error);
     })
 }
 
-module.exports = mailSubscriber
+module.exports = sendEmailProviders
