8db5aff63b745f7a81c5e0e529da207a8caef210
jcorvera <javier.corvra@gmail.com>
2021-02-26 16:08:20 -0600

    refactor app.j

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const config = require('./config/index');
-//const compression = require('compression');
+const compression = require('compression');
 const mongooseConnection = require('./services/servicesUsingMongoose/database/mongoose')
-//const cors = require('cors');
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./api/v1/controllers/index');
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-//app.use(cors());
-//app.use(compression({ filter: shouldCompress }))
+app.use(cors());
+app.use(compression({ filter: shouldCompress }))
 
 app.use('/api/v1/',routes);
 
@@ -22,9 +22,9 @@ app.listen(config.app.port || 8080,() => {
     console.log(`Server ir running in port http://localhost:${config.app.port}`);
 });
 
-/*function shouldCompress (req, res) {
+function shouldCompress (req, res) {
     if (req.headers['x-no-compression']) {
         return false;
     }
     return compression.filter(req, res);
-}*/
+}
