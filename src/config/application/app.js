0b6c1731f0f1b31bd604c2df86aea5010884c653
jcorvera <javier.corvra@gmail.com>
2021-02-26 15:44:08 -0600

    refactor app.js

--- src/config/application/app.js
+++ src/config/application/app.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
     env:  process.env.NODE_ENV,
     name: process.env.NODE_NAME,
-    port: process.env.NODE_PORT,
+    port: process.env.NODE_PORT || 8000,
     host: process.env.NODE_HOST
 }
